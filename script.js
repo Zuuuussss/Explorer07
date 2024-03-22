bb9619aec33ec28d802183453652fd30a6c37cae
Zuuuussss <ti.tift@hotmail.com>
2024-03-22 03:05:44 -0300

    popup

--- script.js
+++ script.js
@@ -3,17 +3,36 @@ const form = document.querySelector("form")
 const inputweight = document.querySelector("#weight")
 const inputheight = document.querySelector("#height")
 
-form.onsubmit = function (event) {
-  event.preventDefault()
+const modalwrapper = document.querySelector(".modal-wrapper")
+const modalmessage = document.querySelector(".modal .tittle span")
+const modalbtnclose = document.querySelector(".modal button.close")
+
+const modal = {
+  open() {
+    modalwrapper.classList.add("open")
+  },
+  close() {
+    modalwrapper.classList.remove("open")
+  },
+}
+
+form.onsubmit = (Event) => {
+  Event.preventDefault()
 
   const weight = inputweight.value
   const height = inputheight.value
 
   const result = IMC(weight, height)
-  console.log(result)
+  const message = `Seu IMC é de ${result}`
+
+  modalmessage.innerText = message
+  modal.open()
+}
+
+modalbtnclose.onclick = () => {
+  modal.close()
 }
 
 function IMC(weight, height) {
-  return (weight / ((height / 100) ** 2)).toFixed(2)
+  return (weight / (height / 100) ** 2).toFixed(2)
 }
-
