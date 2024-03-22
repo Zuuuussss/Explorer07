36ec8b2387d11eb8abdc068a3ed73a64434e59da
Zuuuussss <ti.tift@hotmail.com>
2024-03-22 02:13:25 -0300

    function IMC

--- script.js
+++ script.js
@@ -9,5 +9,11 @@ form.onsubmit = function (event) {
   const weight = inputweight.value
   const height = inputheight.value
 
-  console.log(weight, height)
+  const result = IMC(weight, height)
+  console.log(result)
 }
+
+function IMC(weight, height) {
+  return (weight / ((height / 100) ** 2)).toFixed(2)
+}
+
